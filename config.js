72a4944f7159640b1011cfd9f9220c475379e881
baburahasia <89130764+baburahasia@users.noreply.github.com>
2021-09-02 09:57:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
-  token: "ODM0MDg0MDczMTQzNzk1NzE0.YH7vUw.6iMxHlL3ahA34A1z326H16Kt148",
-  prefix: "po",
-  errChnl: "859481642816110593"
+  token: "TOKEN",
+  prefix: "PREFIX",
+  errChnl: "ERRORLOG"
 }
 
 module.exports = config
